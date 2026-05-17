6bc4e76ebf4a0c46ca7a3555f2c4466e8ff20604
duckpao <nguyenducbao173@gmail.com>
2026-05-18 01:25:41 +0700

    changed folders structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
-const authRoutes = require('./routes/authRoute'); 
+const authRoutes = require('./route/authRoute'); 
 
 const app = express();
 const PORT = 5000;
